46fd3bfa09f752605fee93bb8a731560e33b94fc
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-03-10 10:31:23 -0300

    feat: create state

---
+++ TP1/state.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+class State:
+    def __init__(self, table):
+        self.table = table
+
+    def next_moves(self):
+        childs = []
+        for i in range(3):
+            for j in range(3):
+                if self.table[i][j] == 0:
+                    if i < 2:
+                        childs.append(State(self.swap(i, j, 1, 0)))
+                    if j < 2:
+                        childs.append(State(self.swap(i, j, 0, 1)))
+                    if j > 0:
+                        childs.append(State(self.swap(i, j, 0, -1)))
+                    if i > 0:
+                        childs.append(State(self.swap(i, j, -1, 0)))
+        return childs
+
+    def swap(self, i, j, x, y):
+        table = self.table.copy()
+        table[i][j], table[i + x][j + y] = table[i + x][j + y], table[i][j]
+        return table
+
+    def print(self):
+        for i in range(3):
+            for j in range(3):
+                print(self.table[i][j], end="")
+            print(" ")
+
+    def compare_to(self, state):
+        return np.array_equal(self.table[0], state.table[0]) & np.array_equal(self.table[1], state.table[1]) & np.array_equal(self.table[2], state.table[2])
+
+
+# state = State([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+# state.print()
+# print("-------------")
+# childs = state.next_moves()
+#
+# for i in range(len(childs)):
+#     print(childs[i].print())

64f9e0519e6531bb73f9598b4892f4b2d6ce26c2
Agustin Naso <anaso@itba.edu.ar>
2022-03-25 22:45:47 -0300

    feat: crossbreeding algorithms

--- TP2/individual.py
+++ TP2/individual.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 class Individual:
-    gen = np.array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    def __init__(self):
+        self.gen = np.zeros((11,), dtype=int)
 
     def fitness(self):
         return self.gen[0]

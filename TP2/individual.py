9c0a1330bc31a24863a97e010fd8057bc6ffce7d
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-03-26 11:56:37 -0300

    feat: fitness

--- TP2/individual.py
+++ TP2/individual.py
@@ -1,8 +1,36 @@
 import numpy as np
 
 
-class Individual:
-    gen = np.array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+#                  0  1  2  3   4    5    6  7     8    9    10
+class Individual:  # W0 W1 W2 w11 w12  w13  w21 w22  w23  w01  w02
+    gen = np.array([0, 1,  2,  3, 4,   5,   6,   7,   8,  9,   10])
+
+    xi = np.array([[4.4793, -4.0765, -4.0765],[-4.1793, -4.9218, 1.7664],[-3.9429, -0, 7689, 4.8830]])
+    zeta = np.array([ 0, 1, 1])
+
+    def __init__(self, gen):
+        self.gen = gen
+        self.fitnessValue = self.fitness()
 
     def fitness(self):
-        return self.gen[0]
+        sum = 0
+        for i in range(3):
+            sum += np.float_power((self.zeta[i] - self.f(self.xi[i])), 2)
+        return sum
+
+    def f(self, xi):
+        sum1 = 0
+        sum2 = 0
+        for i in range(2):
+            for j in range(3):
+                sum2 += self.gen[3 + i * 2 + j] * xi[j]
+            sum2 -= - self.gen[9 + i]
+            sum1 += self.gen[i + 1] * g(sum2)
+            sum2 = 0
+        return g(sum1 - self.gen[0])
+
+
+def g(x):
+    return np.float_power(np.e, x) / (1 + np.float_power(np.e, x))
+
+
--- TP2/main.py
+++ TP2/main.py
@@ -2,12 +2,7 @@ import numpy as np
 
 
 def main():
-    Xi1 = np.array([4.4793, -4.0765, -4.0765])
-    Xi2 = np.array([-4.1793, -4.9218, 1.7664])
-    Xi3 = np.array([-3.9429, -0, 7689, 4.8830])
-    z1 = 0
-    z2 = 1
-    z3 = 1
+
     print("Hello World!")
 
 

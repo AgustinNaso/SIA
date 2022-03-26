e4580315a2227eb551c8ad0b8819a304243121e2
linbrittany <brlin@itba.edu.ar>
2022-03-26 18:35:23 -0300

    fix: selection methods

--- TP2/selection.py
+++ TP2/selection.py
@@ -3,12 +3,12 @@ import random
 
 def elite_selection(population):
     # sort list of individuals by fitness
-    population.sort(key=lambda x: x.fitness, reverse=True)
+    population.sort(key=lambda x: x.fitness)
     return population[0:population.size]
 
 
 def truncate_selection(population):
-    population.sort(key=lambda x: x.fitness, reverse=True)
+    population.sort(key=lambda x: x.fitness)
     truncated = population[0:population.size * 0.9]
     return random.sample(truncated, population.size)
 
@@ -22,18 +22,17 @@ def roulette_wheel_selection(population):
 
 
 def rank_selection(population):
-    population.sort(key=lambda x: x.fitness, reverse=True)
+    population.sort(key=lambda x: x.fitness)
     fitness_sum = population.fitness_sum()
     probabilities = []
-    for individual in population.population:
-        probabilities.append((individual.fitness + fitness_sum) / fitness_sum)
+    for i in range(population.size):
+        probabilities.append((i + 1 + fitness_sum) / fitness_sum)
     return select_population(population, probabilities)
 
 
 def select_population(population, probabilities):
     selected = set()
-    n = len(selected)
-    while n < population.size:
+    while len(selected) < population.size:
         i = 0
         p = random.random()
         current = probabilities[i]
@@ -42,7 +41,6 @@ def select_population(population, probabilities):
             i += 1
             current = probabilities[i]
         selected.add(population.population[i])
-        n = len(selected)
     return selected
 
 

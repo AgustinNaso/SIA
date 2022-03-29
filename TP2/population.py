88050d5b0089bd57cd3d1ef57e06389668baa594
linbrittany <brlin@itba.edu.ar>
2022-03-29 17:58:58 -0300

    fix: main algorithm

--- TP2/population.py
+++ TP2/population.py
@@ -7,23 +7,17 @@ class Population:
         self.population = []
         self.generation = 0
 
-    def add_individual(self, individual):
-        self.population.append(individual)
-        return
-
     def min_fitness(self):
         return min(self.population, key=attrgetter('real_fitness')).real_fitness
-
-    def set_new_population(self, new_population):
-        self.population = new_population
-        return
 
     def fitness_sum(self):
         return float(sum(individual.fitness for individual in self.population))
 
-    def new_generation(self):
-        self.generation += 1
-        return
-
     def sort_desc(self):
         self.population.sort(key=lambda x: x.fitness, reverse=True)
+
+    def print_fitness(self):
+        string = ""
+        for individual in self.population:
+            string += str(individual.fitness) + " "
+        print(string)

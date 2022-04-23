aaea8817963ccf9a89c3cb36552251ad7a582b60
linbrittany <brlin@itba.edu.ar>
2022-04-23 20:22:10 -0300

    feat: simple perceptron and exercise 1 test

--- TP3/perceptron/non_linear_perceptron.py
+++ TP3/perceptron/non_linear_perceptron.py
@@ -2,17 +2,20 @@ from perceptron.perceptron import Perceptron
 import numpy as np
 
 
-class SimplePerceptron(Perceptron):
+class NonLinearPerceptron(Perceptron):
 
     def __init__(self, training_set, expected_output, learning_rate):
         super().__init__(training_set, expected_output, learning_rate)
 
     def activation(self, excitation):
-        return 1 if excitation >= 0 else -1
+        return np.tanh(excitation)
 
     def error(self, w):
         error = 0
         for i in range(len(self.training_set)):
             excitation = np.inner(self.training_set[i], w)
-            error += abs(self.expected_output[i] - self.activation(excitation))
-        return error
+            error += (self.expected_output[i] - self.activation(excitation)) ** 2
+        return 0.5 * error
+
+    def activation_derivative(self, excitation):
+        return 1 - np.tanh(excitation)**2
--- TP3/perceptron/perceptron.py
+++ TP3/perceptron/perceptron.py
@@ -1,16 +1,49 @@
 from abc import ABC, abstractmethod
+import numpy as np
 
 
 class Perceptron(ABC):
 
-    def __init__(self, training_set, expected_output):
-        self.training_set = training_set
+    def __init__(self, training_set, expected_output, learning_rate):
+        self.training_set = np.array(list(map(lambda t: np.append(t, [1]), training_set)))
         self.expected_output = expected_output
+        self.learning_rate = learning_rate
         self.error_min = None
         self.w_min = None
 
     # Training algorithm
+    def train(self, epoch):
+        i = 0
+        error = 1
+        p = len(self.training_set)
+        self.error_min = p * 2
+        w = np.zeros(len(self.training_set[0]))
+        while error > 0 and i < epoch:
+            i_x = np.random.randint(1, p)
+            excitation = np.inner(self.training_set[i_x], w)
+            activation = self.activation(excitation)
+            w += self.learning_rate * (self.expected_output[i_x] - activation) * self.training_set[i_x] * self.activation_derivative(excitation)
+            error = self.error(w)
+            if error < self.error_min:
+                self.error_min = error
+                self.w_min = w
+            i += 1
+
+    def get_results(self, test_input):
+        real_input = np.array(list(map(lambda t: np.append(t, [1]), test_input)))
+        results = []
+        for i in range(len(real_input)):
+            excitation = np.inner(real_input[i], self.w_min)
+            results.append(self.activation(excitation))
+        return results
+
     @abstractmethod
-    def train(self):
+    def activation(self, excitation):
         pass
 
+    @abstractmethod
+    def error(self, w):
+        pass
+
+    def activation_derivative(self, excitation):
+        return 1

aeb03499a1072e68e3ca5173b4abfaf0abef7adb
linbrittany <brlin@itba.edu.ar>
2022-06-17 16:20:16 -0300

    feat: mlp helper

--- TP5/helpers/font_helper.py
+++ TP5/helpers/font_helper.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+
+def to_bits(fonts: np.ndarray) -> np.ndarray:
+    results = []
+    for encoded_character in fonts:
+        results.append(to_bin_array(encoded_character))
+    return np.array(results)
+
+
 def to_bin_array(encoded_character: np.ndarray) -> np.ndarray:
     bin_array = np.zeros((7, 5), dtype=int)
     for row in range(0, 7):
@@ -9,12 +17,13 @@ def to_bin_array(encoded_character: np.ndarray) -> np.ndarray:
             current_row >>= 1
     return bin_array
 
+
 def print_letter(array):
     for i in range(7):
         for j in range(5):
-            if array[j+i*5] > 0.5:
-                print("*",end="")
+            if array[j + i * 5] > 0.5:
+                print("*", end="")
             else:
-                print(" ",end="")
+                print(" ", end="")
         print("")
     print("")

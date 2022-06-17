befb33526698283fc5e724f02603298434d8b50e
Agustin Naso <anaso@itba.edu.ar>
2022-06-17 17:20:12 -0300

    feat: heatmat plot of output

--- TP5/ej1a/main.py
+++ TP5/ej1a/main.py
@@ -5,40 +5,46 @@ from TP5.resources.fonts import font_1
 from TP5.helpers.font_helper import to_bin_array, print_letter
 from TP5.utils import create_multilayer_perceptron_and_train
 
+
+def round_array(array):
+    for idx in range(len(array)):
+        array[idx] = 1 if array[idx] > 0.7 else 0
+    return np.array(array).reshape(7, 5)
+
+
 nested_set = np.array(list(map(to_bin_array, font_1)))
 set = np.array(list(map(np.concatenate, nested_set)))
-layers = np.array([35, 25, 15, 5, 2, 5, 15, 25, 35])
+layers = np.array([35, 15, 2, 15, 35])
 learning_rate = 0.0005
-epoch: int = 15000
+epoch: int = 1
 
 perceptron = create_multilayer_perceptron_and_train(set, set, learning_rate, epoch, layers, 100)
 outputs = perceptron.test_input(set)
 # print(output[0])
 # output_hm = np.split(np.array(output[0]), 7)
-# monocromatic_cmap = plt.get_cmap('binary')
+monochromatic_cmap = plt.get_cmap('binary')
 
-for i in range(len(outputs)):
+print(np.array(outputs[0]).reshape(len(outputs[0]), 1).shape)
+print(round_array(outputs[0]))
 
-    # heatmap(
-    #     to_bin_array(font_1[1]),
-    #     linewidths=0.2,
-    #     cbar=False,
-    #     square=True,
-    #     cmap=monocromatic_cmap,
-    #     linecolor='k')
-    # plt.show()
-    print_letter(set[i])
-    print_letter(outputs[i])
+# for i in range(len(outputs)):
+#     # heatmap(
+#     #     to_bin_array(font_1[1]),
+#     #     linewidths=0.2,
+#     #     cbar=False,
+#     #     square=True,
+#     #     cmap=monocromatic_cmap,
+#     #     linecolor='k')
+#     # plt.show()
+#     print_letter(set[i])
+#     print_letter(outputs[i])
 
-
-
-
-# heatmap(
-#     to_bin_array(output_hm),
-#     linewidths=0.2,
-#     cbar=False,
-#     square=True,
-#     cmap=monocromatic_cmap,
-#     linecolor='k')
-# plt.show()
+heatmap(
+    round_array(outputs[0]),
+    linewidths=0.2,
+    cbar=False,
+    square=True,
+    cmap=monochromatic_cmap,
+    linecolor='k')
+plt.show()
 

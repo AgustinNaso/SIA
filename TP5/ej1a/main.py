83aa6544fe04f546ce5e0c1c43a0b42a17f8a750
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-06-17 15:59:52 -0300

    feat: test with all letters

--- TP5/ej1a/main.py
+++ TP5/ej1a/main.py
@@ -7,38 +7,17 @@ from TP5.utils import create_multilayer_perceptron_and_train
 
 nested_set = np.array(list(map(to_bin_array, font_1)))
 set = np.array(list(map(np.concatenate, nested_set)))
-layers = np.array([35, 25, 15, 5, 2, 5, 15, 25, 35])
+layers = np.array([35, 15, 5, 2, 5, 15, 35])
 learning_rate = 0.0005
-epoch: int = 15000
+epoch: int = 5000
 
 perceptron = create_multilayer_perceptron_and_train(set, set, learning_rate, epoch, layers, 100)
 outputs = perceptron.test_input(set)
-# print(output[0])
-# output_hm = np.split(np.array(output[0]), 7)
-# monocromatic_cmap = plt.get_cmap('binary')
+
 
 for i in range(len(outputs)):
-
-    # heatmap(
-    #     to_bin_array(font_1[1]),
-    #     linewidths=0.2,
-    #     cbar=False,
-    #     square=True,
-    #     cmap=monocromatic_cmap,
-    #     linecolor='k')
-    # plt.show()
+    print(set[i])
+    print(outputs[i])
     print_letter(set[i])
     print_letter(outputs[i])
-
-
-
-
-# heatmap(
-#     to_bin_array(output_hm),
-#     linewidths=0.2,
-#     cbar=False,
-#     square=True,
-#     cmap=monocromatic_cmap,
-#     linecolor='k')
-# plt.show()
 

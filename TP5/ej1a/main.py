65e641dda5409a140a340c4a676c8c44180fe962
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-06-17 16:41:41 -0300

    feat: add momentum and change in perceptro architecture

--- TP5/ej1a/main.py
+++ TP5/ej1a/main.py
@@ -6,18 +6,18 @@ from TP5.helpers.font_helper import to_bin_array, print_letter
 from TP5.utils import create_multilayer_perceptron_and_train
 
 nested_set = np.array(list(map(to_bin_array, font_1)))
-set = np.array(list(map(np.concatenate, nested_set)))
-layers = np.array([35, 15, 5, 2, 5, 15, 35])
+sets = np.array(list(map(np.concatenate, nested_set)))
+layers = np.array([35, 20, 2, 20, 35])
 learning_rate = 0.0005
 epoch: int = 5000
 
-perceptron = create_multilayer_perceptron_and_train(set, set, learning_rate, epoch, layers, 100)
-outputs = perceptron.test_input(set)
+perceptron = create_multilayer_perceptron_and_train(sets, sets, learning_rate, epoch, layers, 100, momentum=True)
+outputs = perceptron.test_input(sets)
 
 
 for i in range(len(outputs)):
-    print(set[i])
+    print(sets[i])
     print(outputs[i])
-    print_letter(set[i])
+    print_letter(sets[i])
     print_letter(outputs[i])
 

46b0ba854243ab0bb0a95e78b9bd4fc218d3fd01
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-06-17 20:04:10 -0300

    feat: concept vector plots

--- TP5/ej1a/main.py
+++ TP5/ej1a/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from TP5.helpers.plot_helpers import plot_points
-from TP5.resources.fonts import font_1
+from TP5.resources.fonts import font_1, font_1_header
 from TP5.helpers.font_helper import to_bin_array, print_letter
 from TP5.helpers.mlp_helper import create_multilayer_perceptron_and_train
 
@@ -14,11 +14,20 @@ def round_array(array):
     return np.array(array).reshape(7, 5)
 
 
+def concept_vector(point1, point2, point_amount):
+    delta_0 = point2[0] - point1[0]
+    delta_1 = point2[1] - point1[1]
+    vector = []
+    for i in range(point_amount):
+        vector.append([ point1[0]+delta_0*(i/point_amount), point1[1]+delta_1*(i/point_amount)])
+    return np.array(vector)
+
+
 nested_set = np.array(list(map(to_bin_array, font_1)))
 sets = np.array(list(map(np.concatenate, nested_set)))
 layers = np.array([35, 20, 2, 20, 35])
 learning_rate = 0.0005
-epoch: int = 100
+epoch: int = 10000
 
 perceptron = create_multilayer_perceptron_and_train(sets, sets, learning_rate, epoch, layers, 100, momentum=True)
 midPoints = perceptron.enc_bulk(sets)
@@ -35,27 +44,61 @@ outputs = perceptron.test_input(sets)
 monochromatic_cmap = plt.get_cmap('binary')
 
 plot_points(np.array(midPoints))
-
+plot_points(concept_vector(midPoints[17], midPoints[20], 10))
 
 # for i in range(len(outputs)):
 #     print(sets[i])
 #     print(outputs[i])
 #     print_letter(sets[i])
 #     print_letter(outputs[i])
+heatmap(
+        round_array(sets[17]),
+        linewidths=0.2,
+        cbar=False,
+        square=True,
+        cmap=monochromatic_cmap,
+        linecolor='k')
+plt.title(f"{6} real")
+plt.show()
+heatmap(
+        round_array(sets[20]),
+        linewidths=0.2,
+        cbar=False,
+        square=True,
+        cmap=monochromatic_cmap,
+        linecolor='k')
+plt.title(f"{5} real")
+plt.show()
 
-heatmap(
-    round_array(sets[1]),
-    linewidths=0.2,
-    cbar=False,
-    square=True,
-    cmap=monochromatic_cmap,
-    linecolor='k')
-plt.show()
-heatmap(
-    round_array(outputs[1]),
-    linewidths=0.2,
-    cbar=False,
-    square=True,
-    cmap=monochromatic_cmap,
-    linecolor='k')
-plt.show()
+c_vector = perceptron.dec_bulk(concept_vector(midPoints[17], midPoints[20], 10))
+
+for i in range(len(c_vector)):
+    heatmap(
+        round_array(c_vector[i]),
+        linewidths=0.2,
+        cbar=False,
+        square=True,
+        cmap=monochromatic_cmap,
+        linecolor='k')
+    plt.title(f"{i} real")
+    plt.show()
+
+# for i in range(len(sets)):
+#     heatmap(
+#         round_array(sets[i]),
+#         linewidths=0.2,
+#         cbar=False,
+#         square=True,
+#         cmap=monochromatic_cmap,
+#         linecolor='k')
+#     plt.title(f"{i} real")
+#     plt.show()
+#     heatmap(
+#         round_array(outputs[i]),
+#         linewidths=0.2,
+#         cbar=False,
+#         square=True,
+#         cmap=monochromatic_cmap,
+#         linecolor='k')
+#     plt.title(f"{i} test")
+#     plt.show()

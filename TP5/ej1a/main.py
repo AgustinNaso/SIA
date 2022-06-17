2b202d819654644c7ad185e5f91152df49840bb5
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-06-17 17:58:16 -0300

    feat: enc and dec

--- TP5/ej1a/main.py
+++ TP5/ej1a/main.py
@@ -9,15 +9,24 @@ nested_set = np.array(list(map(to_bin_array, font_1)))
 sets = np.array(list(map(np.concatenate, nested_set)))
 layers = np.array([35, 20, 2, 20, 35])
 learning_rate = 0.0005
-epoch: int = 5000
+epoch: int = 100
 
 perceptron = create_multilayer_perceptron_and_train(sets, sets, learning_rate, epoch, layers, 100, momentum=True)
-outputs = perceptron.test_input(sets)
+# outputs = perceptron.test_input(sets)
+midPoints = perceptron.enc_bulk(sets)
+print(f'element     mid points')
+for i in range(len(midPoints)):
+    print(f'{i}    {midPoints[i]}')
+
+decs = perceptron.dec_bulk(midPoints)
+print(f'element     errors')
+for i in range(len(decs)):
+    print(f'{i}    {sets[i] - decs[i]}')
 
 
-for i in range(len(outputs)):
-    print(sets[i])
-    print(outputs[i])
-    print_letter(sets[i])
-    print_letter(outputs[i])
+# for i in range(len(outputs)):
+#     print(sets[i])
+#     print(outputs[i])
+#     print_letter(sets[i])
+#     print_letter(outputs[i])
 

b96b4544bb8cbcd466a26a07c2790c33edcbc775
linbrittany <brlin@itba.edu.ar>
2022-06-18 12:16:23 -0300

    feat: comparison plot

--- TP5/helpers/font_helper.py
+++ TP5/helpers/font_helper.py
@@ -1,18 +1,49 @@
 import numpy as np
 import random
+from TP5.multilayer_perceptron.multilayer_perceptron import MultilayerPerceptron
+import matplotlib.pyplot as plt
+
+
+def plot_comparison(noisy_set: np.ndarray, expected_output: np.ndarray, mlp: MultilayerPerceptron,
+                    epochs: int, learning_rate: float):
+    images = []
+
+    for i in range(expected_output.shape[0]):
+        images.append(expected_output[i].reshape((7, 5)))
+
+    for j in range(noisy_set.shape[0]):
+        images.append(noisy_set[j].reshape((7, 5)))
+
+    predicted = np.array(mlp.test_input(noisy_set))
+
+    error = (np.average((predicted - expected_output) ** 2)) / len(predicted)
+    print(error)
+
+    for k in range(predicted.shape[0]):
+        images.append(predicted[k].reshape((7, 5)))
+
+    fig, axes = plt.subplots(3, noisy_set.shape[0], figsize=(7, 5))
+    for p, ax in enumerate(axes.flat):
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.imshow(images[p])
+
+    axes[0, 15].set_title("Expected Output", fontsize=20)
+    axes[1, 15].set_title("Input With Noise", fontsize=20)
+    axes[2, 15].set_title("Output", fontsize=20)
+
+    plt.suptitle(f"Epochs: {epochs} - Learning Rate: {learning_rate}", fontsize=25)
+
+    plt.show()
+
 
 
 def add_noise(images: np.ndarray, noise_coverage: float, noise_factor: float) -> np.ndarray:
     noisy_set = np.empty((np.size(images, 0), np.size(images, 1)))
     for i in range(images.shape[0]):
-        noisy_set[i] = np.array([images[i][j] + random.uniform(0, noise_factor) * (1 if images[i][j] == 0 else -1)
+        noisy_set[i] = np.array([images[i][j] + random.uniform(0, noise_factor) * (1 if images[i][j] <= 0 else -1)
                                  if random.uniform(0, 1) < noise_coverage else images[i][j]
                                  for j in range(0, images.shape[1])])
-        # for j in range(images.shape[1]):
-        #     apply = random.uniform(0, 1)
-        #     noisy_input[i][j] = images[i][j]
-        #     if apply < noise_coverage:
-        #         noisy_input[i][j] = images[i][j] + random.uniform(0, noise_factor) * (1 if images[i][j] == 0 else -1)
     return noisy_set
 
 
